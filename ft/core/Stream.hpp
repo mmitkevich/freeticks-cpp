4e32d23df779aa691fbca82c165ce5663d71a941
mmitkevich <mmitkevich@rencap.com>
2020-10-07 01:30:17 +0300

    core: Stream

--- ft/core/Stream.hpp
+++ ft/core/Stream.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include "ft/utils/Common.hpp"
+#include "toolbox/util/Slot.hpp"
 
 namespace ft::core {
 
 template<typename MessageT>
-class Stream {
+class Stream : public tbu::Signal<const MessageT&> {
+    using Base=tbu::Signal<const MessageT&>;
 public:
-
+    using Base::Base;
+    using Base::connect;
+    using Base::disconnect;
 };
 
 };
